86cc108a77db38cd771c7d41bed4392012a155aa
agservizi <ag.servizi16@gmail.com>
2026-01-14 22:59:42 +0100

    Aggiungi il pulsante di logout nella barra di navigazione per migliorare l'accessibilità

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,13 +4,16 @@
             <i class="bi bi-list"></i>
         </button>
 
-        <div class="ms-auto">
+        <div class="ms-auto d-flex align-items-center gap-2">
             <form class="d-flex" id="searchForm">
                 <input class="form-control me-2" type="search" placeholder="Cerca cliente..." id="searchCliente">
                 <button class="btn btn-outline-primary" type="submit">
                     <i class="bi bi-search"></i>
                 </button>
             </form>
+            <a href="/logout.php" class="btn btn-outline-danger">
+                <i class="bi bi-box-arrow-right"></i> Logout
+            </a>
         </div>
     </div>
 </nav>
