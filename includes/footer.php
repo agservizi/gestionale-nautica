566c059f03eb4c61e3d145fa4e30cfda016d171e
agservizi <ag.servizi16@gmail.com>
2026-02-08 18:05:36 +0100

    Rimuovi lo stile personalizzato dal footer e ripristina il layout predefinito

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
     </div> <!-- /wrapper -->
 
-    <footer class="app-footer py-3">
+    <footer class="py-3 border-top bg-white">
         <div class="container-fluid">
             <div class="d-flex flex-column flex-md-row align-items-center justify-content-between small text-muted">
                 <div>
